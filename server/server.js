1014fa27b8e787f379884582e56f60ddb2226df0
Chen Jie <61531625+eijnehc@users.noreply.github.com>
2022-10-19 22:50:21 +0800

    Rename server

--- server/server.js
+++ server/server.js
@@ -5,15 +5,15 @@ const cors = require('cors');
 const app = express();
 const PORT = 8000;
 const CLIENT_PORT = 5173;
+const VITE_STRIPE_KEY =
+  'sk_test_51LqtYuIwH8bO08yNtcROZ97p1fRFCeZQhakkeBWtwSisoVMcd83R8dLIf4ISX840Q0l4GdJVAT2zycLUEHab39dS00AQ6m5TK1';
 const PRICE_ID = 'price_1LqwRHIwH8bO08yN5SDxo6ZX';
 
 app.use(cors());
 app.use(express.json());
 app.use(express.static('public'));
 
-const stripe = initStripe(
-  'sk_test_51LqtYuIwH8bO08yNtcROZ97p1fRFCeZQhakkeBWtwSisoVMcd83R8dLIf4ISX840Q0l4GdJVAT2zycLUEHab39dS00AQ6m5TK1'
-);
+const stripe = initStripe(VITE_STRIPE_KEY);
 
 app.post('/api/v1/create-checkout-session', async (req, res) => {
   const session = await stripe.checkout.sessions.create({
@@ -24,8 +24,8 @@ app.post('/api/v1/create-checkout-session', async (req, res) => {
       },
     ],
     mode: 'payment',
-    success_url: `http://localhost:${CLIENT_PORT}/welcome?session_id={CHECKOUT_SESSION_ID}`,
-    cancel_url: `http://localhost:${CLIENT_PORT}/welcome`,
+    success_url: `http://localhost:${CLIENT_PORT}/login?session_id={CHECKOUT_SESSION_ID}`,
+    cancel_url: `http://localhost:${CLIENT_PORT}/welcome?canceled=true`,
   });
 
   res.redirect(303, session.url);
@@ -38,5 +38,5 @@ app.get('/api/v1/order/success', async (req, res) => {
 });
 
 app.listen(PORT, () => {
-  console.log('Server is running');
+  console.log(`Server port ${PORT} is running`);
 });
