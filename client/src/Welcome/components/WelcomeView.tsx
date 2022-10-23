7bc29f8112e822e3b6c846b22392d857bab3d7b3
Chen Jie <61531625+eijnehc@users.noreply.github.com>
2022-10-23 17:15:57 +0800

    Refactor code

--- client/src/Welcome/components/WelcomeView.tsx
+++ client/src/Welcome/components/WelcomeView.tsx
@@ -1,49 +1,162 @@
 import { FC } from 'react'
+import { Aperture, Codepen, Map } from 'react-feather'
 import styled from 'styled-components'
 
-import { ThemeProvider } from 'styled-components'
-
 import { apiRoutes } from '../../apiRoutes'
+import { Logo } from '../../global'
+import RocketLaunch from '../assets/rocket-launch.png'
 import BackgroundImage from '../assets/stars.png'
-import { Header } from './Header.jsx'
-import Card from './Card.jsx'
-import { Container } from './styles/Container.styled.js'
-import content from './Content.js'
-import Footer from './Footer'
 
-const theme = {
-  colors: {
-    header: '#ebfbff',
-    body: '#fff',
-    footer: '#003333',
-  },
-  mobile: '768px',
-}
+import { CourseHighlights } from './Constants'
 
 export const WelcomeView: FC = () => {
+  const renderImage = (id: number) => {
+    switch (id) {
+      case 1:
+        return <Aperture size='80px' />
+      case 2:
+        return <Codepen size='80px' />
+      case 3:
+        return <Map size='60px' />
+      default:
+        null
+    }
+  }
   return (
-    <ThemeProvider theme={theme}>
-      <Wrapper>
-        <Header />
-        <Container>
-          {content.map((item, index) => (
-            <Card key={index} item={item} />
+    <Wrapper>
+      <HeroWrapper>
+        <BoxContent>
+          <Header>Code Confidently Like a Pro</Header>
+          <p>
+            RockCat is where students find fun after-school activities, that empower them to design, code,
+            create, and spark a passion, so they can go and change the world with a smile on their face.
+          </p>
+          <br />
+          <form action={apiRoutes.createCheckoutSessionHttpUrl} method='POST'>
+            <Button>Get Started</Button>
+          </form>
+        </BoxContent>
+        <img src={RocketLaunch} alt='rocket-launch' />
+      </HeroWrapper>
+      <CourseHighlightsWrapper>
+        <Header style={{ color: 'var(--color-primary-dark)' }}>What We Provide</Header>
+        <CourseHighlightsContent>
+          {CourseHighlights.map((item) => (
+            <Card key={item.title}>
+              <ImageWrapper>{renderImage(item.id)}</ImageWrapper>
+              <CardTitle>{item.title}</CardTitle>
+              <p>{item.body}</p>
+            </Card>
           ))}
-        </Container>
-        <hr></hr>
-        <Footer />
-        <form action={apiRoutes.createCheckoutSessionHttpUrl} method='POST'>
-          <button>Checkout</button>
-        </form>
-      </Wrapper>
-    </ThemeProvider>
+        </CourseHighlightsContent>
+      </CourseHighlightsWrapper>
+      <FooterWrapper>
+        <div>
+          <Logo size='24px' />
+          &nbsp;RockCat
+        </div>
+        <p>Unlock your potential | Master the Code</p>
+        <p style={{ color: 'var(--color-gray-500)' }}>&copy; 2022 RockCat. All rights reserved.</p>
+      </FooterWrapper>
+    </Wrapper>
   )
 }
 
 const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
   min-height: 100%;
+  width: 100%;
   background-color: var(--color-offblack);
+  color: var(--color-white);
   background-image: url(${BackgroundImage});
+`
+
+const HeroWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  padding-left: 150px;
+  margin-bottom: 48px;
+`
+
+const Header = styled.header`
+  font-size: 1.5rem;
+  font-weight: var(--font-weight-bold);
+  margin-bottom: 32px;
+`
+
+const BoxContent = styled.div`
+  width: clamp(200px, 40%, 400px);
+  max-width: 100%;
+`
+
+const Button = styled.button`
+  color: inherit;
+  background-color: var(--color-primary-medium);
+  border-radius: 2rem;
+  border: none;
+  font-size: 1rem;
+  cursor: pointer;
+  padding: 1rem 3rem;
+  font-weight: 700;
+  opacity: 0.9;
+
+  &:hover {
+    opacity: 1;
+    transform: scale(1.05);
+  }
+`
+
+const CourseHighlightsWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  align-items: center;
+`
+
+const CourseHighlightsContent = styled.main`
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+  gap: 32px;
+`
+
+const Card = styled.article`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  flex: 1 1 250px;
+  max-width: 350px;
+  justify-self: center;
+  text-align: justify;
+  padding: 2rem;
+`
+
+const ImageWrapper = styled.div`
+  display: grid;
+  place-content: center;
+  background-color: var(--color-secondary-dark);
+  height: 100px;
+  width: 100px;
+  border-radius: 50%;
+  margin-bottom: 16px;
+`
+
+const CardTitle = styled.div`
+  color: var(--color-secondary-medium);
+  font-weight: var(--font-weight-bold);
+  margin-bottom: 16px;
+`
+
+const FooterWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  border-top: 1px solid var(--color-gray-700);
+  padding: 16px;
+  gap: 8px;
 `
 
 WelcomeView.displayName = 'WelcomeView'
