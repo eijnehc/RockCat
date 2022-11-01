01ffc02dd77854383a756008a4733bc9559b67b3
kunnathvineeth <vineethkunnathsg@gmail.com>
2022-11-01 22:59:13 +0800

    canvas context simplified

--- client/src/Dashboard/components/Map/GameComponents/canvasContext.ts
+++ client/src/Dashboard/components/Map/GameComponents/canvasContext.ts
@@ -1,14 +1,8 @@
 import React from 'react';
 
-interface IGridPosition {
-  X: number,
-  Y: number
-}
-
 export interface ICanvasContext {
     renderingContext: CanvasRenderingContext2D | null,
     saveRenderingContext: (ctx?: CanvasRenderingContext2D) => void
-    //postion: IGridPosition
 }
 
 const CanvasContext = React.createContext<ICanvasContext | null>(null);
